e41fd03daa352f0c8ce50e7f1b7128801a15af7e
Sinan Ozturk <sinansinan_@outlook.com.tr>
2025-02-23 00:59:15 +0300

    spring boot--ürün getirme

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -1,16 +1,40 @@
 package kodlamaio.northwind.entities.concretes;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
-
+@Entity
 @Data
+@Table(name="products")
 public class Product {
-
+	
+	@Id
+	@GeneratedValue
+    @Column(name="product_id")
 	private int id;
+    
+    @Column(name="category_id")
 	private int categoryId;
+    
+    @Column(name="product_name")
 	private String productName;
+    
+    @Column(name="unit_price")
 	private double unitPrice;
+    
+    @Column(name="units_in_stock")
 	private short unitsInStock;
+    
+    @Column(name="quantity_per_unit")
 	private String quantityPerUnit;
+    
+    public Product() {
+    	
+    }
+    
 	public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock,
 			String quantityPerUnit) {
 		super();
@@ -20,6 +44,6 @@ public class Product {
 		this.unitPrice = unitPrice;
 		this.unitsInStock = unitsInStock;
 		this.quantityPerUnit = quantityPerUnit;
-	}//yapılandırıcı parametreli ve global degişkene eşitlenir
+	}
 
 }
