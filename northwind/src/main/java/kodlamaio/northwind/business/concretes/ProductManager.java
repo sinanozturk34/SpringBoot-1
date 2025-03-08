3c500a2470af40631d9ecbec55b2309d6c598197
Sinan Ozturk <sinansinan_@outlook.com.tr>
2025-03-08 16:05:54 +0300

    core.utilities.results-->Result-DataResult

--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
 @Service
@@ -20,8 +23,10 @@ public class ProductManager implements ProductService{
 	}
 	
 	@Override
-	public List<Product> getAll() {
-		return this.productDao.findAll();
+	public  DataResult<List<Product>> getAll() {
+		return new SuccessDataResult<List<Product>>
+		(this.productDao.findAll(),"Data listelendi");
+				
 	}
    
 }
