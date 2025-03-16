7c68b7a2c414a01c7ae587277c43594786890b84
Sinan Ozturk <sinansinan_@outlook.com.tr>
2025-03-16 19:21:57 +0300

    @PostMapping

--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -2,15 +2,20 @@ package kodlamaio.northwind.business.concretes;
 
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.business.abstracts.ProductService;
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
+
+
 @Service
 public class ProductManager implements ProductService{
 
@@ -27,6 +32,12 @@ public class ProductManager implements ProductService{
 		return new SuccessDataResult<List<Product>>
 		(this.productDao.findAll(),"Data listelendi");
 				
+	}
+
+	@Override
+	public Result add(Product product) {
+		this.productDao.save(product);
+		return new SuccessResult("Ürün eklendi");
 	}
    
 }
--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -3,6 +3,7 @@ package kodlamaio.northwind.entities.concretes;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -12,14 +13,14 @@ import lombok.Data;
 public class Product {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="product_id")
 	private int id;
     
     @Column(name="category_id")
 	private int categoryId;
     
-    @Column(name="product_name")
+    @Column(name = "product_name")
 	private String productName;
     
     @Column(name="unit_price")
