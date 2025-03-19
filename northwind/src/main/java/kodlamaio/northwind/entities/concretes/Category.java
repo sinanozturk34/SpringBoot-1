58ced7eac52473686f0055ef40c70ae405edcb25
Sinan Ozturk <sinansinan_@outlook.com.tr>
2025-03-19 15:18:49 +0300

    Tablolar arası ilişki

---
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Category.java
@@ -0,0 +1,30 @@
+package kodlamaio.northwind.entities.concretes;
+
+
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="categories")
+@Entity
+public class Category {
+	@Id
+	@Column(name="category_id")
+    private int categoryId;
+	@Column(name="category_name")
+    private String categoryName;
+	
+	@OneToMany(mappedBy = "categories")
+	private List<Product> products;
+}
